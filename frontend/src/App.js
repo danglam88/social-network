0085d6c22fbe7fd1866dd5c59728b82805b824ee
jo-eman <johannes.eckerman@gritlab.ax>
2023-04-18 13:41:11 +0300

    added websocketservice, notificationservice, and notification icon component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,39 @@ import LoginForm from "./components/LoginForm";
 import PersonalProfile from "./components/PersonalProfile";
 import Chat from "./components/GroupChat";
 import GroupList from "./components/GroupList";
+import WebSocketService from "./services/WebSocketService";
+import NotificationIcon from "./components/NotificationIcon";
 
 function App() {
   let token = "";
+  let wsurl = "ws://localhost:8080/ws";
 
   if (document.cookie.includes("session_token")) {
     const tokens = document.cookie.split(";");
     for (let i = 0; i < tokens.length; i++) {
       if (tokens[i].includes("session_token")) {
-        token = tokens[i].split("=")[1]
+        token = tokens[i].split("=")[1];
+        break; 
       }
     }
   }
 
+  if (token !== "") {
+    // changed condition to check if token is not empty
+    WebSocketService.connect(wsurl);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {token !== "" && ( 
+        <header className="App-header">
+          <div>
+            <NotificationIcon />
+          </div>
+        </header>
+      )}
+
+      <div className="App-body"> {}
         {token !== "" ? (
           <div>
             <PersonalProfile />
@@ -34,7 +51,7 @@ function App() {
             <LoginForm />
           </div>
         )}
-      </header>
+      </div>
     </div>
   );
 }

323e5650b5083a25317fe5538a4a2f8784bcc10a
Dang Lam <dang.lam@gritlab.ax>
2023-05-10 20:24:27 +0300

    updated docker_clean script

--- docker_clean.sh
+++ docker_clean.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -e
 
-# Stop and remove all frontend containers
-cd frontend/
-docker stop frontend-container
-docker system prune -f --volumes
-
-# Stop and remove all backend containers
-cd ../backend/
-docker stop backend-container
+# Stop and remove all containers and images
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
 docker system prune -f --volumes
